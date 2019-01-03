a21287c1fadae7b70bec44eb6f05c137e074e5bd
Hector <hectorarias8716@gmail.com>
2019-01-03 18:04:48 -0500

    Implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'angular', to: 'portfolios#angular'
+  get 'ruby-on-rails', to: 'portfolios#rails'
 
   resources :blogs do
   	member do
